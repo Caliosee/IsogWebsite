6febdc81be27f001e6cc3701d14eb66dacbf3677
Caliosee <120561100+Caliosee@users.noreply.github.com>
2022-12-15 12:57:02 +0800

    Update Product.js

--- pages/Product.js
+++ pages/Product.js
@@ -1,7 +1,6 @@
 import Navigationproduct from "../componentsforproduct/Navigationproduct";
 import Productproduct from "../componentsforproduct/Productproduct";
 import Footerproduct from "../componentsforproduct/Footerproduct";
-import Iconproduct from "../componentsforproduct/iconproduct";
 
 export default function Productpage() {
   return (
